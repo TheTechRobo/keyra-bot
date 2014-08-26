a2ea04a841a40dc3494aa9adeb01fb7d8073fe40
Thanatermesis <thanatermesis@gmail.com>
2014-08-26 13:20:26 +0200

    improved script

--- scripts/keyrabot.sh
+++ scripts/keyrabot.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 main(){
     # pre {{{
-    local var
+    local sources_dir="$HOME/keyra-bot"
 
-    mkdir -p "$HOME/keyra/bot"
-    mkdir -p "$HOME/keyra/database"
+    mkdir -p "$sources_dir/database"
 
     # }}}
 
     while true
     do
         # we need to run it from his directory itself, in order to save files in its databases
-        cd "$HOME/keyra-bot/database"
+        cd "$sources_dir/database"
 
         # run the bo
-        "$HOME/keyra-bot/bot/keyrabot.rb"
+        "$sources_dir/bot/keyrabot.rb"
 
         # bot died by any unknown reason ? wait a bit and run it again
         sleep 30
