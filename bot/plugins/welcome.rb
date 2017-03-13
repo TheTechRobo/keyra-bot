c02d5a88b5bea8cd83d36925ff16e3f9fdfef03a
Thanatermesis <thanatermesis@gmail.com>
2017-03-13 21:54:19 +0100

    udpated welecome mesage

--- bot/plugins/welcome.rb
+++ bot/plugins/welcome.rb
@@ -16,12 +16,12 @@ module Cinch
           sleep 5
 
           if m.user.nick != bot.nick && m.user.nick =~ /(EliveWeb)/
-            m.reply "welcome #{m.user.nick}! If you have any problem running Elive please report it to http://bugs.elivecd.org so we can assist and having you notified about it easier. You can also talk to Prince-AMD or Thanaterme-sis users"
+            m.reply "welcome #{m.user.nick}! If you have any problem with Elive just talk to Thanatermesi if is here or report it to http://bugs.elivecd.org to better assist and having you notified about it easier."
           else
             if m.user.nick != bot.nick && m.user.nick =~ /(Elive_user)/
               m.reply "welcome to Elive, #{m.user.nick}! =)"
               sleep 8
-              m.reply "if you want to leave us a report about Elive leave us your email too so we can contact you in case we need more details!"
+              m.reply "how is your experience with Elive so far? any suggestion or issue to report us so we can make it better? for a direct assistance just talk to Thanatermesi or use http://bugs.elivecd.org , your feedbacks is important for improve Elive and your reports are for the correct work for everybody!"
             else
               # Reply !
               m.reply "welcome #{m.user.nick} =)"
