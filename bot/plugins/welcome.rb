1bcdf4fbfbcb6863f6320793953345aab3d2bfe5
Thanatermesis <thanatermesis@gmail.com>
2017-08-03 22:13:16 +0200

    check if user is online before to say hi

--- bot/plugins/welcome.rb
+++ bot/plugins/welcome.rb
@@ -16,21 +16,29 @@ module Cinch
           sleep 5
 
           if m.user.nick != bot.nick && m.user.nick =~ /(EliveWeb)/
-            m.reply "welcome #{m.user.nick}! If you have any problem with Elive just talk to Thanatermesi if is here or report it to http://bugs.elivecd.org to better assist and having you notified about it easier."
+            if m.user.online
+              m.reply "welcome #{m.user.nick}! If you have any problem with Elive just talk to Thanatermesi if is here or report it to http://bugs.elivecd.org to better assist and having you notified about it easier."
+            end
           else
 
             if m.user.nick != bot.nick && m.user.nick =~ /(Elive_user)/
-              m.reply "welcome to Elive, #{m.user.nick}! =)"
+              if m.user.online
+                m.reply "welcome to Elive, #{m.user.nick}! =)"
+              end
               sleep 8
-              m.reply "how is your experience with Elive so far? any suggestion or issue to report us so we can make it better? for a direct assistance just talk to Thanatermesi or use http://bugs.elivecd.org , your feedbacks is important for improve Elive and your reports are for the correct work for everybody!"
+              if m.user.online
+                m.reply "how is your experience with Elive so far? any suggestion or issue to report us so we can make it better? for a direct assistance just talk to Thanatermesi or use http://bugs.elivecd.org , your feedbacks is important for improve Elive and your reports are for the correct work for everybody!"
+              end
             else
 
               if m.user.nick != bot.nick && m.user.nick =~ /(Elive_inst)/
-                m.reply "welcome to the Elive world, #{m.user.nick}! =) we hope you enjoy this system"
+                if m.user.online
+                  m.reply "welcome to the Elive world, #{m.user.nick}! =) we hope you enjoy this system"
+                end
                 sleep 8
-                m.reply "if you have any suggestion or issue to report just tell us, so we can make even better this system for everybody! you can also use http://bugs.elivecd.org  Oh btw! press control-s to set your real nickname :)"
-                # Reply !
-                m.reply "welcome #{m.user.nick} =)"
+                if m.user.online
+                  m.reply "if you have any suggestion or issue to report just tell us, so we can make even better this system for everybody! you can also use http://bugs.elivecd.org  Oh btw! press control-s to set your real nickname :)"
+                end
               end
             end
 
