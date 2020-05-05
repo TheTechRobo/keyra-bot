99c02885aa6c38f6d9d26d1a9cff7bbf395f6d5a
Ittussarom Retals Mail Ynohtna <52163910+TheTechRobo@users.noreply.github.com>
2020-05-05 19:16:08 -0400

    grammar

--- bot/plugins/welcome.rb
+++ bot/plugins/welcome.rb
@@ -17,7 +17,7 @@ module Cinch
           if m.user.nick != bot.nick && m.user.nick =~ /(EliveWeb)/
             if m.user.online
               #sleep 2
-              m.reply "welcome #{m.user.nick}! Ask your question! Have you see the new amazing forums too? http://forum.elivelinux.org , the are gorgeous! =)"
+              m.reply "Welcome #{m.user.nick}! Ask your question! Have you see the new amazing forums too? http://forum.elivelinux.org , they are gorgeous! =)"
             end
           else
 
@@ -29,7 +29,7 @@ module Cinch
 
               sleep 6
               if m.user.online
-                m.reply "how is your experience with Elive so far? any suggestion or issue to report us? your feedbacks are important for improve Elive for everybody! try also the new amazing forums! https://forum.elivelinux.org"
+                m.reply "How is your experience with Elive so far? Any suggestions or issues to report to us? Your feedback is so important to improve Elive for everybody! PS: try out the new amazing forums! https://forum.elivelinux.org"
               end
 
             else
@@ -37,11 +37,11 @@ module Cinch
               if m.user.nick != bot.nick && m.user.nick =~ /(Elive_inst)/
                 sleep 2
                 if m.user.online
-                  m.reply "welcome to the Elive world, #{m.user.nick}! =) I hope you are enjoying the system!"
+                  m.reply "Welcome to the Elive world, #{m.user.nick}! =) I hope you are enjoying the system!"
                 end
                 sleep 6
                 if m.user.online
-                  m.reply "You can find the amazing Elive community on this new BETA Forum website! https://forum.elivelinux.org , BTW press control-s to set your nickname here :)"
+                  m.reply "You can find the amazing Elive community on this new forum website! https://forum.elivelinux.org. BTW, press Control-S to set your nickname here :)"
                 end
               end
             end
